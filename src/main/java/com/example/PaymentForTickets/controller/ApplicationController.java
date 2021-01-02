dec70cbb1bf7e1f0ba53d0444e63a59d98c641f6
Evgeniy <46program@gmail.com>
2021-01-02 20:07:10 +0200

    Added slf4j logging

--- src/main/java/com/example/PaymentForTickets/controller/ApplicationController.java
+++ src/main/java/com/example/PaymentForTickets/controller/ApplicationController.java
@@ -3,6 +3,8 @@ package com.example.PaymentForTickets.controller;
 import com.example.PaymentForTickets.domain.Application;
 import com.example.PaymentForTickets.domain.Status;
 import com.example.PaymentForTickets.repository.ApplicationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,8 @@ public class ApplicationController {
 
     private final ApplicationRepository repository;
 
+    Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+
 
     public ApplicationController(ApplicationRepository repository) {
         this.repository = repository;
@@ -26,18 +30,20 @@ public class ApplicationController {
     public Application acceptingApplications(@PathVariable("num") Integer num,
                                              @RequestParam(name = "date", required = false)
                                              @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime dateTime) {
-        System.out.println(LocalDateTime.now());
+
+        logger.debug("Local datetime {}", dateTime);
         Application application = new Application();
         application.setRouteNumber(num);
         application.setDataTime(dateTime);
         application.setStatus(Status.PROCESSED);
+        logger.debug("Application {}", application);
         return application;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Long createApplication(@Valid @RequestBody Application application) {
-      return repository.save(application).getId();
+        return repository.save(application).getId();
     }
 
 }
--- src/main/java/com/example/PaymentForTickets/controller/StatusController.java
+++ src/main/java/com/example/PaymentForTickets/controller/StatusController.java
@@ -4,6 +4,8 @@ package com.example.PaymentForTickets.controller;
 import com.example.PaymentForTickets.domain.Application;
 import com.example.PaymentForTickets.domain.Status;
 import com.example.PaymentForTickets.repository.ApplicationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -20,6 +22,9 @@ public class StatusController {
     private WebClient webClient;
 
     private final ApplicationRepository repository;
+
+    Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+
 
     public StatusController(ApplicationRepository repository) {
         this.repository = repository;
@@ -65,38 +70,38 @@ public class StatusController {
 
 
     @GetMapping("/")
-    public Mono<Application> getStatus(){
+    public Mono<Application> getStatus() {
         List<Application> all = repository.findAll();
         while (true) {
-                try {
-                    for (Application application : all) {
-                        if (application.getStatus() != null && application.getStatus().equals(Status.PROCESSED)) {
-                            System.out.println("Ok");
-                            Long id = application.getId();
-                           return webClient
-                                    .put()
-                                    .uri("new/{id}", id)
-                                    .retrieve()
-                                    .bodyToMono(Application.class);
-                        }
+            try {
+                for (Application application : all) {
+                    if (application.getStatus() != null && application.getStatus().equals(Status.PROCESSED)) {
+                        Long id = application.getId();
+                        logger.debug("Application id = {}", id);
+                        return webClient
+                                .put()
+                                .uri("new/{id}", id)
+                                .retrieve()
+                                .bodyToMono(Application.class);
                     }
-                    Thread.sleep(1000*60);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
                 }
+                Thread.sleep(1000 * 60);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
             }
+        }
     }
 
 
     @PutMapping("new/{id}")
-    public Application setStatus(@PathVariable("id") Application application) {
+    public Status setStatus(@PathVariable("id") Application application) {
         Application one = repository.getOne(application.getId());
         List<Status> statuses = List.of(Status.values());
         int size = statuses.size();
         Random random = new Random();
         one.setStatus(statuses.get(random.nextInt(size)));
-        System.out.println(one.getStatus());
-        return repository.save(one);
+        logger.debug("Status random {}", one.getStatus());
+        return repository.save(one).getStatus();
     }
 
 
